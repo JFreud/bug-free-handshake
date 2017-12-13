0fc408adb953fcfab11bd118db2133e0ef912544
JFreud <jrm.frdnbrg@gmail.com>
2017-12-13 12:09:47 -0500

    finished handshake

--- pipe_networking.c
+++ pipe_networking.c
@@ -14,31 +14,37 @@ int server_handshake(int *to_client) {
   //int READ = 0;
   //int WRITE = 1;
 
-  printf("hello\n");
+  printf("server started\n");
   mkfifo("WKP", 0644);
-  printf("Second hello\n");
+  //printf("Second hello\n");
   int pfd = open("WKP", O_RDONLY, 0644);
   if (pfd == -1) {
     printf("%s\n", strerror(errno));
   }
-  printf("WKP fd: %d\n", pfd);
+  printf("Server: WKP Created\n");
   char * pvtfifo = malloc(512);
+  //printf("pvt start\n");
   read(pfd, pvtfifo, sizeof(pvtfifo));
+  //printf("read end\n");
+
+
 
   if(strcmp(pvtfifo, "priv") == 0){
+    printf("Server: Received connection\n");
     close(pfd);
     //mkfifo("ser_to_cli", 0644);
     *to_client = open(pvtfifo, O_WRONLY);
-    printf("%d\n", *to_client);
+    //printf("%d\n", *to_client);
     char * testmsg = "yes";
     write(*to_client, testmsg, sizeof(testmsg));
+    printf("Server: Sent message\n");
 
   }
 
   char * confirmed = malloc(512);
   read(pfd, confirmed, sizeof(confirmed));
   if (strcmp(confirmed, "confirmed")) {
-    printf("Handshake shaken");
+    printf("Server: Handshake confirmed\n");
   }
 
   free(pvtfifo);
@@ -59,23 +65,31 @@ int server_handshake(int *to_client) {
   =========================*/
 int client_handshake(int *to_server) {
 
-  printf("Client hello\n");
+  //printf("Client hello\n");
   *to_server = open("WKP", O_WRONLY, 0644);
-  printf("%d\n", *to_server);
+  //printf("%d\n", *to_server);
+  char * sent= "priv";
+  write(*to_server, sent, sizeof(sent));
+  printf("Client: Sent connection\n");
+
 
   mkfifo("priv", 0644);
   int pfd = open("priv", O_RDONLY, 0644);
-  printf("%d\n", pfd);
-  char * sent= "priv";
-  write(*to_server, sent, sizeof(sent));
+  if (pfd == -1){
+    printf("%s\n", strerror(errno));
+  }
+  //printf("%d\n", pfd);
+
 
   // int pfd1 = open("ser_to_cli", O_RDONLY);
   char * message = malloc(512);
   read(pfd, message, sizeof(message));
 
   if (strcmp(message, "yes") == 0) {
+    printf("Client: Received message\n");
     close(pfd);
     char * confirm = "confirmed";
+    printf("Client: Sending confirmation\n");
     write(*to_server, confirm, sizeof(confirm));
   }
 
