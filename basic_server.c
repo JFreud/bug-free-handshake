39a8aa37ef40e6ba04e71390d338d2a1bf6fd8b2
JFreud <jrm.frdnbrg@gmail.com>
2017-12-14 10:41:33 -0500

    changed it to capitalize--works now

--- basic_server.c
+++ basic_server.c
@@ -8,13 +8,14 @@ int main() {
 
   //char * usermod = malloc(512);
   from_client = server_handshake( &to_client );
-
+  char usermod[512];
   while (1) {
     int ret;
-    char * usermod = malloc(512);
+    //char * usermod = malloc(512);
     read(from_client, usermod, sizeof(usermod));
     printf("Here it is: %s\n", usermod);
-    usermod = strcat(usermod, " repeats the server");
+    //usermod = strcat(usermod, " repeats the server");
+    *usermod = toupper(*usermod);
     //printf("toclient: %d\n", to_client);
     printf("Modified stuff: %s\n", usermod);
     if ((ret = write(to_client, usermod, sizeof(usermod))) == -1) {
